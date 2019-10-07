f7b3b21ecd335ef7986343cdc50fcf48e951d34a
germanheinz <heinz.german@gmail.com>
2019-10-07 17:51:14 +0200

    Cors

--- src/main/java/com/springbootangular/api/controller/ClienteController.java
+++ src/main/java/com/springbootangular/api/controller/ClienteController.java
@@ -3,12 +3,14 @@ package com.springbootangular.api.controller;
 import com.springbootangular.api.model.Cliente;
 import com.springbootangular.api.service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api")
 public class ClienteController {
